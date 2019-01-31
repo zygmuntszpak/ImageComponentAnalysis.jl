db4693c39fe896635e7d479d6cd0f5f05679e364
Abbey McCarthy <abbey.mccarthy932@gmail.com>
2019-01-31 14:18:02 +1030

    Adds Moore tracing and outer tracing algorithms

--- src/ImageComponentAnalysis.jl
+++ src/ImageComponentAnalysis.jl
@@ -1,13 +1,20 @@
 module ImageComponentAnalysis
 
 using Images
+using PaddedViews
 
 abstract type ComponentAnalysisAlgorithm end
 struct ContourTracing <: ComponentAnalysisAlgorithm end
+struct MooreTracing <: ComponentAnalysisAlgorithm end
+struct OuterTracing <: ComponentAnalysisAlgorithm end
 
 include("contour_tracing.jl")
+include("moore_tracing.jl")
+include("outer_tracing.jl")
 
 export
     label_components,
-	ContourTracing
+	ContourTracing,
+	MooreTracing,
+	OuterTracing
 end # module
