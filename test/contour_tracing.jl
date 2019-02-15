4ef0752d47b2d456ff42a9ff11ff4fb844f1d232
Wesley <wesleytws@gmail.com>
2019-02-15 13:10:56 +1030

    Fix spacing

--- test/contour_tracing.jl
+++ test/contour_tracing.jl
@@ -1,15 +1,15 @@
 
 @testset "contour_tracing" begin
 
-	for T in (Int, Bool, Gray{Bool}, Gray{N0f8}, Gray{N0f16}, Gray{N0f32}, Gray{Float64})
-		for i = 1:20
-			test_image = T.(eval(Symbol("test_image_$(i)")))
+    for T in (Int, Bool, Gray{Bool}, Gray{N0f8}, Gray{N0f16}, Gray{N0f32}, Gray{Float64})
+        for i = 1:20
+            test_image = T.(eval(Symbol("test_image_$(i)")))
 
-			# Call contour tracing algorithm and compare the number of components.
-			labels = ImageComponentAnalysis.label_components(ContourTracing(), test_image)
-			num_components = maximum(labels)
-			@test num_components == test_image_results[i]
-		end
-	end
+            # Call contour tracing algorithm and compare the number of components.
+            labels = ImageComponentAnalysis.label_components(ContourTracing(), test_image)
+            num_components = maximum(labels)
+            @test num_components == test_image_results[i]
+        end
+    end
 
 end
